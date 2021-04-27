5e2850f9978b1c6719be9b3106ee23e80db995cd
Ryooooooga <eial5q265e5@gmail.com>
2021-04-27 21:14:43 +0900

    colored error output

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Ryooooooga/zouch
 go 1.16
 
 require (
+	github.com/fatih/color v1.10.0
 	github.com/stoewer/go-strcase v1.2.0
 	github.com/urfave/cli/v2 v2.3.0
 )
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/Ryooooooga/zouch/pkg/cli"
+	"github.com/fatih/color"
 )
 
 var (
@@ -15,6 +16,6 @@ var (
 
 func main() {
 	if err := cli.New(fmt.Sprintf("%s (rev: %s) built at %s", version, commit, date)).Run(os.Args); err != nil {
-		panic(err)
+		color.New(color.FgRed).Fprintf(os.Stderr, "zouch: %s\n", err)
 	}
 }
