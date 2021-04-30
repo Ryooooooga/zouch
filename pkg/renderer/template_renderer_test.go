a3256384a06beba1fd3d766e4c3a63d997d8f1a2
Ryooooooga <eial5q265e5@gmail.com>
2021-04-30 21:18:46 +0900

    add tests for template renderer

---
+++ pkg/renderer/template_renderer_test.go
@@ -0,0 +1,67 @@
+package renderer_test
+
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/Ryooooooga/zouch/pkg/renderer"
+	"github.com/Ryooooooga/zouch/pkg/repositories"
+)
+
+func newTestTextTemplateRenderer() *renderer.TextTemplateRenderer {
+	r := renderer.NewTextTemplateRenderer()
+	r.FuncMap["Now"] = func() time.Time {
+		return time.Date(2021, 4, 30, 20, 45, 30, 0, time.Local)
+	}
+
+	return r
+}
+
+func TestRenderTemplate(t *testing.T) {
+	scenarios := []struct {
+		testname       string
+		templateText   string
+		data           interface{}
+		expectedOutput string
+	}{
+		{
+			testname:       "call-now",
+			templateText:   `Today is {{ Now.Format "2006-01-02" }}`,
+			data:           nil,
+			expectedOutput: "Today is 2021-04-30",
+		},
+		{
+			testname:       "variable",
+			templateText:   `data.foo == {{ .foo }}`,
+			data:           map[string]string{"foo": "BAR"},
+			expectedOutput: "data.foo == BAR",
+		},
+		{
+			testname:       "string",
+			templateText:   `{{ . | UpperSnakeCase }}`,
+			data:           "Hello world!",
+			expectedOutput: "HELLO_WORLD!",
+		},
+	}
+
+	for _, s := range scenarios {
+		t.Run(s.testname, func(t *testing.T) {
+			r := newTestTextTemplateRenderer()
+			output := bytes.NewBufferString("")
+
+			tpl := repositories.TemplateFile{
+				Path:    s.testname,
+				Content: []byte(s.templateText),
+			}
+
+			if err := r.RenderTemplate(output, tpl, s.data); err != nil {
+				t.Fatalf("t.RenderTemplate() returns an error %v", err)
+			}
+
+			if result := output.String(); result != s.expectedOutput {
+				t.Fatalf("result != %s, actual %s", s.expectedOutput, result)
+			}
+		})
+	}
+}
